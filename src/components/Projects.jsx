1b7e2e59fa2867bdae481094ae9f410e638f5312
Muhammad Ali <amuhammad161@gmail.com>
2022-06-27 20:54:40 +0500

    added new projects and updated cv

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -61,15 +61,15 @@ const Projects = () => {
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React Wiki Search
+                React Codepen Clone
               </span>
               <div className='pt-8 text-center'>
-                <a target="_blank" rel="noopener noreferrer" href='https://ali-vx.github.io/react-wiki-search/'>
+                <a target="_blank" rel="noopener noreferrer" href='https://ali-vx.github.io/react-codepen-clone/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a target="_blank" rel="noopener noreferrer" href='https://github.com/ali-vx/react-wiki-search'>
+                <a target="_blank" rel="noopener noreferrer" href='https://github.com/ali-vx/react-codepen-clone'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -120,12 +120,17 @@ const Projects = () => {
             className='bg-WeatherImg hover:bg-WeatherImgHover hover:scale-105 duration-500 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
+            <div className='opacity-0 group-hover:opacity-100 text-center'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                Flutter Weather App
+                React Movies and TV Show Watchlist
               </span>
               <div className='pt-8 text-center'>
-                <a target="_blank" rel="noopener noreferrer" href='https://github.com/ali-vx/flutter_weather_app'>
+                <a target="_blank" rel="noopener noreferrer" href='https://ali-vx.github.io/react-watchlist/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href='https://github.com/ali-vx/react-watchlist'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
